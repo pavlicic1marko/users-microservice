8289c7ea08a705e0b8c7c2c0688f45a17e3a1b86
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-04-19 12:39:41 +0200

    adding rest framework, adding api view , for "product" and "product/id" urls

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.getRoutes, name='routes')
+    path('', views.getRoutes, name='routes'),
+    path('/products', views.getProducts, name='products'),
+    path('/products/<str:pk>', views.getProductsById, name='products')
+
+
 ]
