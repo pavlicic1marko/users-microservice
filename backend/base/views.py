18a5e3de5276690d85ff8cb5cf9702a882faf844
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-04-26 15:28:06 +0200

    jwt customization

--- backend/base/views.py
+++ backend/base/views.py
@@ -3,17 +3,35 @@ from django.http import JsonResponse
 from .products import products
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        token_data = super().validate(attrs)
+        token_data['username'] = self.user.username
+        token_data['email'] = self.user.email
+
+        return token_data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 
 # Create your views here.
 
 @api_view(['GET'])
 def getRoutes(request):
-    routes= ['api/products','api/products/<id>']
+    routes = ['api/products', 'api/products/<id>']
     return Response(routes)
+
 
 @api_view(['GET'])
 def getProducts(request):
     return Response(products)
+
 
 @api_view(['GET'])
 def getProductsById(request, pk):
