3304744713a6ddb4e16e4fd53a9d217c190fade6
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-04-30 12:41:05 +0200

    serialize user, return in profile url response

--- backend/base/views.py
+++ backend/base/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-
+from .serializer import UserSerializer
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
@@ -31,6 +31,15 @@ def getRoutes(request):
 @api_view(['GET'])
 def getProducts(request):
     return Response(products)
+
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+
+    serializer = UserSerializer(user, many=False)
+
+
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
