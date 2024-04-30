0dc65e86881bc2fe260f1b82001e461d4ccacf51
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-04-30 18:01:32 +0200

    adding token as a field to user API

--- backend/base/serializer.py
+++ backend/base/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from  rest_framework_simplejwt.tokens import RefreshToken
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -26,3 +27,32 @@ class UserSerializer(serializers.ModelSerializer):
     def get_isAdmin(self, obj):
         return obj.is_staff
 
+
+
+class UserSerializerWithToken(serializers.ModelSerializer): #Used after updating user information
+    token = serializers.SerializerMethodField(read_only=True)
+    name = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'username' ,'name' , '_id', 'isAdmin', 'token']
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return  str(token)
+
+    def get_name(self, obj):
+        name = obj.first_name
+        if name == '':
+            name = obj.email
+
+        return name
+
+    def get__id(self, obj):
+        _id = obj.id
+        return _id
+
+    def get_isAdmin(self, obj):
+        return obj.is_staff
--- backend/base/views.py
+++ backend/base/views.py
@@ -5,15 +5,17 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializer import UserSerializer
+from .serializer import UserSerializer, UserSerializerWithToken
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
-        token_data = super().validate(attrs)
-        token_data['username'] = self.user.username
-        token_data['email'] = self.user.email
+        data = super().validate(attrs)
+        serializer = UserSerializerWithToken(self.user).data
 
-        return token_data
+        for k,v in serializer.items():
+            data[k] = v
+
+        return data
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
