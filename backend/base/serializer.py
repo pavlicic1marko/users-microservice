09e30a4be29718f01cd6c4a3da6a40035eee65a8
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-04-30 14:43:19 +0200

    if name is blank, return email value for name field in api

--- backend/base/serializer.py
+++ backend/base/serializer.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+
 class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = User
-        fields = ['id','email','username']
+        fields = ['id', 'email', 'username' ,'name']
+
+    def get_name(self, obj):
+        name = obj.first_name
+        if name == '':
+            name = obj.email
+
+        return name
+
