9bc973a122dd555946a625743acdd16cc58c80ad
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-05-02 22:12:32 +0200

    refactoring views

--- backend/users/views_jwt.py
+++ backend/users/views_jwt.py
@@ -1,13 +1,8 @@
-from django.db.backends import sqlite3
-from django.shortcuts import render
-from django.http import JsonResponse
-from .products import products
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializer import UserSerializer, UserSerializerWithToken
+from .serializer import UserSerializerWithToken
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import  make_password
 from rest_framework import status
@@ -31,10 +26,7 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
 # Create your views here.
 
-@api_view(['GET'])
-def getRoutes(request):
-    routes = ['api/products', 'api/products/<id>']
-    return Response(routes)
+
 
 @api_view(['POST'])
 
@@ -57,31 +49,4 @@ def registerUser(request):
 
 
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def getProducts(request):
-    return Response(products)
 
-
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def getUserProfile(request):
-    user = request.user
-    serializer = UserSerializer(user, many=False)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-@permission_classes([IsAdminUser])
-def getUsers(request):
-    users = User.objects.all()
-    serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def getProductsById(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
-    return Response(product)
