abbe36220e7f481de7228bf15cf48bde2f6299fc
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-05-02 19:57:25 +0200

    update username, to be the same as email

--- backend/base/signals.py
+++ backend/base/signals.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 
 def upadteUser(sender, instance, **kwargs):
     print('Signal triggered', instance.email)
+    user = instance
+    if user.email != '':
+        user.username = user.email
 
 
 pre_save.connect(upadteUser, sender=User)
