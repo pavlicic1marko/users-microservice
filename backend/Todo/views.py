118a6a65e800fbbccd91c4b8be09f3e9fdcf82ef
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-05-13 23:43:38 +0200

    adding products to TODO microservice

--- backend/Todo/views.py
+++ backend/Todo/views.py
@@ -1,12 +1,26 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from .products import products
 
 # Create your views here.
 
 @api_view(['GET'])
-#@permission_classes([IsAuthenticated])
+@permission_classes([IsAuthenticated])
 def getRoutes(request):
     routes= ['api/products','api/products/<id>']
     return Response(routes)
 
+@api_view(['GET'])
+#@permission_classes([IsAuthenticated])
+def getProducts(request):
+    return Response(products)
+
+@api_view(['GET'])
+def getProductsById(request, pk):
+    product = None
+    for i in products:
+        if i['_id'] == pk:
+            product = i
+            break
+    return Response(product)
