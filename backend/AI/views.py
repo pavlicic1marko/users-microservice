e88138b0e2381fa4e3cf82630b178294cd918102
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-05-15 00:07:02 +0200

    register AI url, adding test url and view

--- backend/AI/views.py
+++ backend/AI/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 # Create your views here.
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes= ['api/AI/test','api/AI/test/<id>']
+    return Response(routes)
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/', include('Todo.urls')),
+    path('api/', include('AI.urls')),
 
 ]
