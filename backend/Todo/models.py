a4dae97529c9d47b9fbc2e7b8ace8fc516b0afa5
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-05-21 11:24:48 +0200

    adding todo model

--- backend/Todo/models.py
+++ backend/Todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    user_id = models.IntegerField(null=False, blank=False)
